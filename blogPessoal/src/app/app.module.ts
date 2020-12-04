f2ae869c9f0c3f31c0bf0e42e9715649674c291f
gustavosouza10 <gsgsouza8@gmail.com>
2020-12-04 10:46:01 -0300

    Componentes de cadastro e login

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {HttpClientModule} from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
